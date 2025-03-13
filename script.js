c0a72c6c6d5d6dc39b5c84150b6c2cd3e2af26a9
Victor Souza <victorsouzavieira.22@gmail.com>
2025-03-13 18:32:34 -0300

    quarto commit

--- script.js
+++ script.js
@@ -8,24 +8,33 @@ function listaNome(){
     } 
 
     let item = document.createElement('li')
+    item.className = "Item"
+    let divItem = document.createElement('div')
+    divItem.className = "divItem"
+    let BtnExcluir = document.createElement('button')
+    BtnExcluir.className = "BtnExcluirLi"
     let nome = document.createElement('h2')
-    item.className = ('Item');
-    nome.className = ('nameItem')
-    item.appendChild(nome);
+    nome.innerHTML = (" ") + input.value
+    nome.className  = "nameItem"
+
+    divItem.appendChild(nome)
+    divItem.appendChild(BtnExcluir)
+    item.appendChild(divItem)
     lista.appendChild(item);
-    item.innerHTML = ('- ') + input.value;
+    console.log
+    // item.innerHTML = ('- ') + input.value;
 }
 
  function formatarNome (){
-    const nomeFormatado = nome.split(" ");
+    const nomeFormatado = document.querySelector = ('nameItem').split(" ")
     for(let i = 0; i < nomeFormatado.length; i++){
         nomeFormatado[i] = nomeFormatado[i][0].toUpperCase() + nomeFormatado[i].substr(1);
-}
+
     nomeFormatado.join(" ");
     nomeFormatado.className('nomeFormatado')
-
     listaFormatar.appendChild(nomeFormatado)
-    nomeFormatado.innerHTML =('- ') + listaFormatar
+    nomeFormatado.innerHTML = ('- ') + listaFormatar
+ }
  }
 
 function adicionarNome (e) {
@@ -36,7 +45,12 @@ function adicionarNome (e) {
 }
     input.addEventListener('keyup', adicionarNome)
 
+function deletarListaFormatada(){
+    listaFormatar.innerHTML = ''
+}
+
     function deletarLista(){
         lista.innerHTML = '';
     }
 
+ 
